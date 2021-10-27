f8fd85a18d6155f31c8739b57234f440a6292770
cmtidmarsh <cmtidmarsh@gmail.com>
2021-10-27 15:28:00 -0400

    f

--- client2/public/in-app.js
+++
@@ -1,8 +0,0 @@
-function h(e) {
-    $(e).css({'height':'65px','overflow-y':'hidden'}).height(e.scrollHeight-30);
-  }
-  $('textarea').each(function () {
-    h(this);
-  }).on('input', function () {
-    h(this);
-  });
