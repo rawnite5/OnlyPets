652e57f6ec13c962d06b4ed1392c71bf2f34a15f
cmtidmarsh <cmtidmarsh@gmail.com>
2021-12-08 16:06:19 -0500

    messages

--- client/src/components/feed/Messages.js
+++ client/src/components/feed/Messages.js
@@ -81,29 +81,13 @@ const Messages = ({ setPage }) => {
 						<input type="text" id="searchForUsersToMessage" placeholder="Search"></input>
 
 						<ul id="myUL">
-							<li><a href="#">Adele</a></li>
-							<li><a href="#">Agnes</a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
 
-							<li><a href="#">Billy</a></li>
-							<li><a href="#">Bob</a></li>
-
-							<li><a href="#">Calvin</a></li>
-							<li><a href="#">Christina</a></li>
-							<li><a href="#">Cindy</a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
 						</ul>
 					</div>
-				<div class="userFiltering-Container">
-					<h3 id="userFilteringTitle">Search for Other Pets...</h3>
-					<input type="text" id="searchForUsersToMessage" placeholder="Search"></input>
-
-					<ul id="myUL">
-						<li><a href="#"></a></li>
-						<li><a href="#"></a></li>
-
-						<li><a href="#"></a></li>
-						<li><a href="#"></a></li>
-					</ul>
-				</div>
 				</div>
 
 
@@ -137,5 +121,25 @@ function toggle_light_mode() {
 function send() {
 	alert("message sent");
 }
+
+// function myFunction() {
+// 	// Declare variables
+// 	var input, filter, ul, li, a, i, txtValue;
+// 	input = document.getElementById('myInput');
+// 	filter = input.value.toUpperCase();
+// 	ul = document.getElementById("myUL");
+// 	li = ul.getElementsByTagName('li');
+  
+// 	// Loop through all list items, and hide those who don't match the search query
+// 	for (i = 0; i < li.length; i++) {
+// 	  a = li[i].getElementsByTagName("a")[0];
+// 	  txtValue = a.textContent || a.innerText;
+// 	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+// 		li[i].style.display = "";
+// 	  } else {
+// 		li[i].style.display = "none";
+// 	  }
+// 	}
+//   }
 
 export default Messages;
